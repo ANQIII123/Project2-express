820a0cc0ef1bdd9370d98360b44ee2ec03354a81
ANQIII123 <liu.anqi@trentglobal.edu.sg>
2022-07-07 21:35:14 +0000

    corrected some codes

--- express/index.js
+++ express/index.js
@@ -7,6 +7,8 @@ require('dotenv').config()
 async function main(){
 let app = express();
 
+app.use(express.json());
+
 app.set("view engine", "hbs");
 app.use(express.static("public"));
 
@@ -18,9 +20,17 @@ await MongoUtil.connect(process.env.MONGO_URL, 'cico');
 
 
 //routes.各种渠道
-app.get('/',function(req,res){
-    res.send("<h1>Welcome to our page</h1>");
+app.get('/', function(req, res){
+    res.json({"test": "test"})
 })
+
+// route to get all covers
+
+// route to post a cover
+
+// route to update a cover
+
+// route to delete a cover
 
 app.get('/contact-us',function(req,res){
     res.send("<h1>Contact us at 81235748</h1>");
@@ -30,9 +40,6 @@ app.get('/contact-us/:email',function(req,res){
     res.send("xxxx@gmail.com"+req.params.email);
 })
 
-app.get('/', function(req, res){
-    res.render('index.hbs');
-})
 
 app.get('/addpiano_sheet', (req,res)=>{
     res.render('addpiano_sheet')
